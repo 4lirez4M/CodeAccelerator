94a8810a02cd639900a14c1ae4b5c4c4560546ea
4l4n <a.r.9632244@gmail.com>
2023-10-10 14:09:01 +0330

    Exercise 7

---
+++ Exercise7.py
@@ -0,0 +1,12 @@
+from datetime import date, datetime
+
+date_components = input("Enter date formatted as YYYY-MM-DD: ").split("-")
+year, month, day = [int(item) for item in date_components]
+d = date(year, month, day)
+today = date.today()
+diffDate = today - d
+if today >= d:
+    print(f"{diffDate.days} days have passed")
+else:
+    diffDate = d - today
+    print(f"{diffDate.days} days left")
